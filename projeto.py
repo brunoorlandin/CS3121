5b56de04cc1bde13fd1fa36e42498a0cbda850f3
bruno <brunoorlandin35@gmail.com>
2020-09-07 23:17:06 +0000

    finalizou parte 3 do projeto

--- projeto.py
+++ projeto.py
@@ -1,7 +1,62 @@
 import os
+import math
 
 def sep():
-    print("\n=======================================\n")
+    print("\n===========================================\n")
+
+def classificaOnda(c):
+    op = int(input("Entre com: \n1 - Numero de onda\n2 - Frequencia angular\nEscolha: "))
+    if op == 1:
+        no = float(input("Numero de onda (em rad/m): "))
+        comprimento = (2 * math.pi)/no
+        frequencia = (2 * math.pi * c)/no
+    elif op == 2:
+        fa = float(input("Frequencia angular (em rad/s): "))
+        frequencia = fa/(2 * math.pi)
+        comprimento = (fa * c)/(2 * math.pi)
+    else:
+        print("Opcao invalida! Digite uma opcao valida.")
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Frequencia: %.2e Hz" %frequencia)
+    
+    if comprimento > 0.3:
+        print("Comprimento de onda = %.2e m" %comprimento)
+        print("Tipo de onda: onda de radio")
+    elif comprimento > 0.001:            
+        print("Comprimento de onda = %.2e mm" % (comprimento*(10**3)))
+        print("Tipo de onda: micro-onda")
+    elif comprimento > 0.0000007:
+        print("Comprimento de onda = %.2e nm" % (comprimento*(10**9)))
+        print("Tipo de onda: infravermelho")
+    elif comprimento > 0.0000004:
+        print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
+        print("Tipo de onda: luz visivel")
+    elif comprimento > 0.00000003:
+        print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
+        print("Tipo de onda: ultravioleta")
+    elif comprimento > 0.00000000003:
+        print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
+        print("Tipo de onda: raio-x")
+    else:
+        print("Comprimento de onda = %.2e nm" % (comprimento * (10**9)))
+        print("Tipo de onda: raio gama")
+
+def numeroOnda (c):
+    op = int(input("Escolha:\n1 - Frequencia\n2 - Comprimento de onda\nEscolha:  "))
+    if op == 1:
+        f = float(input("Digite a freuquencia em Hz: "))
+        no = (2 * math.pi * c)/f
+        fa = 2 * math.pi * f
+    elif op == 2:
+        co = float(input("Digite o comprimento de onda em m: "))
+        no = (2 * math.pi)/co
+        fa = (2 * math.pi * co)/c
+    else:
+        print("Opcao invalida! Digite uma opcao valida.")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Numero de onda: %.2e rad/m" %no)
+    print("Frequencia angular: %.2e rad/s" %fa)
 
 def campoMagMax (c):
     ceMax = float(input("Digite o valor do campo eletrico maximo em V/m: "))
@@ -18,45 +73,40 @@ def campoEleMax (c):
 def comprimento (c):
     while True:
         f = float(input("Digite a frequencia em Hz: "))
-        if f > 0.3:
+        comprimento = c/f
+        if comprimento > 0.3:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e m" % (c/f))
+            print("Comprimento de onda = %.2e m" %comprimento)
             print("Tipo de onda: onda de radio")
             break
-        elif f > (10**-3) or f < 0.3:
-            f = float(input("Digite a frequencia em Hz: "))
+        elif comprimento > 0.001:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e mm" % ((c/f)*(10**3)))
+            print("Comprimento de onda = %.2e mm" % (comprimento*(10**3)))
             print("Tipo de onda: micro-onda")
             break
-        elif f > 7*(10**-7) or f < (10**-3):
-            f = float(input("Digite a frequencia em Hz: "))
+        elif comprimento > 0.0000007:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e nm" % ((c/f)*(10**9)))
+            print("Comprimento de onda = %.2e nm" % (comprimento*(10**9)))
             print("Tipo de onda: infravermelho")
             break
-        elif f > 7*(10**-7) or f < 4*(10**-7):
-            f = float(input("Digite a frequencia em Hz: "))
+        elif comprimento > 0.0000004:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e nm" % ((c/f)*(10**9)))
+            print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
             print("Tipo de onda: luz visivel")
             break
-        elif f > 4*(10**-7) or f < 3*(10**-8):
-            f = float(input("Digite a frequencia em Hz: "))
+        elif comprimento > 0.00000003:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e nm" % ((c/f)*(10**9)))
+            print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
             print("Tipo de onda: ultravioleta")
             break
-        elif f > 3*(10**-8) or f < 3*(10**-11):
-            f = float(input("Digite a frequencia em Hz: "))
+        elif comprimento > 0.00000000003:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e nm" % ((c/f)*(10**9)))
+            print("Comprimento de onda = %.2e nm" % (comprimento *(10**9)))
             print("Tipo de onda: raio-x")
             break
-        elif f < 3 * (10**-11):
-            f = float(input("Digite a frequencia em Hz: "))
+        else:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("Comprimento de onda = %.2e nm" % ((c/f)*(10**9)))
+            print("Comprimento de onda = %.2e nm" % (comprimento * (10**9)))
             print("Tipo de onda: raio gama")
             break
 
@@ -111,7 +161,7 @@ e = 1.602 * (10**-19) #carga elementar em C
 
 def main ():
     while True:
-        op = int(input("Escolha o calculo ou saia:\n1 - Frequencia\n2 - Comprimento de onda:\n3 - Campo magnetico maximo\n4 - Campo eletrico maximo\n0 - Sair\nEscolha:  "))
+        op = int(input("Escolha o calculo ou saia:\n1 - Frequencia\n2 - Comprimento de onda\n3 - Campo magnetico maximo\n4 - Campo eletrico maximo\n5 - Numero de onda e frequencia angular\n6 - Classificar olhando a equacao senoidal\n0 - Sair\nEscolha:  "))
         if op == 1:
             frequencia(c)
         elif op == 2:
@@ -120,6 +170,10 @@ def main ():
             campoMagMax(c)
         elif op == 4:
             campoEleMax(c)
+        elif op == 5:
+            numeroOnda(c)
+        elif op == 6:
+            classificaOnda(c)
         elif op == 0:
             break
         else:
